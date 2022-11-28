d6418d3defc95b020b0fc23653ba19a74329a838
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-11-28 06:43:24 -0800

    INFRA: Initialize Build Project

---
+++ HR.API.Infrastructure.Build/Program.cs
@@ -0,0 +1,71 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPiple = new GithubPipeline
+{
+    Name = "HR Build Pipline",
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "master" }
+        },
+        Push = new PushEvent
+        {
+            Branches = new string[] { "master" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2022,
+
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Checking Out Code"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Seting Up .NET",
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "6.0.403"
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restoring Nuget Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Build Project"
+                },
+
+                new TestTask
+                {
+                    Name = "Running Tests"
+                }
+            }
+        }
+    }
+};
+
+var client = new ADotNetClient();
+
+client.SerializeAndWriteToFile(
+    adoPipeline: githubPiple,
+    path: "../../../../.github/workflows/dotnet.yml");
