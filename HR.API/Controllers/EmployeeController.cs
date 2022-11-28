b5d999299555c21ee83c0e9bd9bff910d71a34cc
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-11-28 05:19:17 -0800

    CODE RUB: Add Header and Remove Empty Line

--- HR.API/Controllers/EmployeeController.cs
+++ HR.API/Controllers/EmployeeController.cs
@@ -5,8 +5,6 @@
 
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace HR.API.Controllers
 {
     [Route("api/[controller]")]
--- HR.API/Program.cs
+++ HR.API/Program.cs
@@ -1,15 +1,16 @@
+//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
