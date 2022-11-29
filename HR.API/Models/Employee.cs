232e8bfbd811640c7f08e7f8c759740b0cabf1c0
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-11-29 01:06:50 -0800

    DATA: Add Validation To Models Employee

--- HR.API/Models/Employee.cs
+++ HR.API/Models/Employee.cs
@@ -3,13 +3,21 @@
 // Free To Use To Find Comfort and Pease
 //===================================================
 
+using System.ComponentModel.DataAnnotations;
+
 namespace HR.API.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string FullName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Department { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
