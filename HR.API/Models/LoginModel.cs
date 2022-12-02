b8e17f6d3fe6abe4f2590095d1a5ea029272d433
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-02 02:29:05 -0800

    Auuth Login -> PASS

---
+++ HR.API/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using System.ComponentModel.DataAnnotations;
+
+namespace HR.API.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
