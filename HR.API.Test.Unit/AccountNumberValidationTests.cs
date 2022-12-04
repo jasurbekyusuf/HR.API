de271923f0bf84d4912a8b18cf4e7876f0d89f66
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-04 07:12:05 -0800

    AccountNumberValidationTests Initialize

---
+++ HR.API.Test.Unit/AccountNumberValidationTests.cs
@@ -0,0 +1,55 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using HR.API.Services;
+using Xunit;
+
+namespace HR.API.Test.Unit
+{
+    public class AccountNumberValidationTests
+    {
+        private readonly AccountNumberValidationService validationSvc;
+
+        public AccountNumberValidationTests() => validationSvc = new AccountNumberValidationService();
+
+        [Fact]
+        public void IsValidAccountNumberReturnsTrue() 
+        {
+            Assert.True(validationSvc.IsValid("123-1234567890-98"));
+        }
+
+        [Theory]
+        [InlineData("1234-1234567890-98")]
+        [InlineData("1-1234567890-98")]
+        public void IsValidFirstPartIsWrongReturnsFalse(string accountNumber) 
+        {
+            Assert.False(validationSvc.IsValid(accountNumber));
+        }
+
+        [Theory]
+        [InlineData("1234-12df567890-98")]
+        [InlineData("1-12345678214324543590-98")]
+        public void IsValidMiddlePartIsWrongReturnsFalse(string accountNumber)
+        {
+            Assert.False(validationSvc.IsValid(accountNumber));
+        }
+
+        [Theory]
+        [InlineData("123-1234567890-9")]
+        [InlineData("123-1234567890 - 98")]
+        public void IsValidLastPartIsWrongReturnsFalse(string accountNumber)
+        {
+            Assert.False(validationSvc.IsValid(accountNumber));
+        }
+
+        [Theory]
+        [InlineData("123-1234567890 19")]
+        [InlineData("123+1234567890 - 98")]
+        public void IsValidDelimetorsThrowsArgumentException(string accountNumber)
+        {
+            Assert.Throws<ArgumentException>(() => validationSvc.IsValid(accountNumber));
+        }
+    }
+}
